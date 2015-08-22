2b6213399d33ba767bc06405618438f230e81d90
Lawrence Abaeo <labaeo@gmail.com>
2015-08-22 12:44:28 -0700

    Fix #20. Add audio via audio HTML5 audio element and autoplay. Note that mobile browsers disable autoplay for audio.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Play the loading and ding noise:
+  // Audio code adapted from: http://jsfiddle.net/gD4Dr/35/
+  var audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', "../audio/loading-ding.mp3");
+  audioElement.setAttribute('autoplay', 'autoplay');
+  audioElement.play();
+  
+  // Convert the page to show 'loading' stuff:
   $(".loading").addClass("spinner");
   $(".rank-number").addClass("no-viz");
   $(".current-rank-badge").addClass("hide");
   $(".admin-message").html("It is under search&#183;&#183;&#183;");
   
-  setTimeout(function() {
+  // Run these after 1500 milliseconds:
+  setTimeout(function () {
     $(".loading").removeClass("spinner");
     $(".rank-number").removeClass("no-viz");
     $(".current-rank-badge").removeClass("hide").addClass("nudge-to-show");
